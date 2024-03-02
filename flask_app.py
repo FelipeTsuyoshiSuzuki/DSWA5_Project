34fa1e2bad8a010c6c51554b9553518071fd8997
Felipe Tsuyoshi Suzuki <ftsuyoshisuzuki@gmail.com>
2024-03-02 18:52:08 -0300

    Adiciona tratamento de erro
    Adiciona o tratamento de erro para o status code 404 e 500

--- flask_app.py
+++ flask_app.py
@@ -25,4 +25,12 @@ def requisition(name):
     dados = {"navegador": user_agent, "request": request, "name": name}
     return render_template('requisition.html', dados = dados)
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
 
